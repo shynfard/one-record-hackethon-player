e726eae58b48f3c142ccf480113394df6bed029e
Your Name <you@example.com>
2022-05-22 13:55:37 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { useEffect, useState } from 'react';
+import { fetchToken, onMessageListener } from './utils/firebase';
+import toast, { Toaster } from 'react-hot-toast';
 
-function App() {
+// ----------------------------------------------------------------------
+
+export default function App() {
+  const [notification, setNotification] = useState({ title: '', body: '' });
+  const [isTokenFound, setTokenFound] = useState(false);
+  fetchToken(setTokenFound);
+  useEffect(() => {
+    console.log(isTokenFound);
+  }, [isTokenFound]);
+  onMessageListener()
+    .then((payload: any) => {
+      setNotification({ title: payload.notification.title, body: payload.notification.body });
+      console.log('Received background message ', payload);
+
+      toast(`${payload.notification.title}: ${payload.notification.body}`);
+    })
+    .catch((err) => console.log('failed: ', err));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Toaster />
+      <ScrollToTop />
+      <Router />
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/pages/Overview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactPlayer from 'react-player/lazy';
+import PropTypes from 'prop-types';
+// icons
+import { Icon, IconProps } from '@iconify/react';
+// @mui
+import { Box, Stack, SxProps, Theme, Typography } from '@mui/material';
+import JoLPlayer from 'jol-player';
+
+// ----------------------------------------------------------------------
+
+interface Props {}
+
+export default function Overview({}: Props) {
+  return (
+    <Box sx={{ m: '0 auto' }}>
+      <Stack spacing={2} sx={{ alignItems: 'center' }}>
+        <Typography align="center" variant="h1">
+          Welcome to our Video Player{' '}
+        </Typography>
+        <Box width="720px" height="480px" sx={{ m: '0 auto' }}>
+          <video width="720" height="480" controls>
+            <source src="https://storage.googleapis.com/random-storage-staging/Sequence%2002_1.mp4" type="video/mp4" />
+          </video>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
